9b70f87c9ea7aa74f331cc87c3df78c61dd1ab24
Nicoalas Tito <mrnicolastito@icloud.com>
2020-02-13 09:25:52 +0000

    Paginação com paramentros

--- src/main/java/com/trabalho/cursomc/services/CategoriaService.java
+++ src/main/java/com/trabalho/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.trabalho.cursomc.domain.Categoria;
@@ -44,5 +47,10 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos!");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
