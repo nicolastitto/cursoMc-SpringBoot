e7070c353b18de4bb20db75fc5f1c4fa04a352da
Nicoalas Tito <mrnicolastito@icloud.com>
2020-02-16 16:44:37 +0000

    Atualização

--- src/main/java/com/trabalho/cursomc/domain/AreaUrbana.java
+++ src/main/java/com/trabalho/cursomc/domain/AreaUrbana.java
@@ -8,10 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_area_urbana")
 public class AreaUrbana implements Serializable {
 	private static final long serialVersionUID = 1L;
 
